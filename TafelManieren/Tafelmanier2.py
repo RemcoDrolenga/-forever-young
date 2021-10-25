8c567e6de811c5ba6980106d8ea6e054aca5de14
RemcoDrolenga <rdrolenga@gmail.com>
2021-10-25 13:22:47 +0200

    Alle opdrachten afgerond

---
+++ TafelManieren/Tafelmanier2.py
@@ -0,0 +1,5 @@
+for i in range(30, -1, -1):
+    print(i)
+
+if i == 0:
+    print("Lancering!")
---
+++ TafelManieren/Tafelmanier3.py
@@ -0,0 +1,5 @@
+for u in range(1, 13):
+    print(u,"AM")
+
+for u in range(1, 13):
+    print(u,"PM")
